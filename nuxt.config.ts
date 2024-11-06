a998fc6868c6742447fcc65320ee28b2d3e52377
ponksak <160387217+ponksak@users.noreply.github.com>
2024-11-06 13:32:04 +0700

    Initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,14 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  postcss:{
+    plugins:{
+      tailwindcss:{},
+      autoprefixer:{},
+      css:['@assets/css/main.css']
+    },
+  },
+
+
+  compatibilityDate: '2024-04-03',
+  devtools: { enabled: true }
+})
